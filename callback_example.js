cf663a5252311d64d16602a9cec818fc16ab450b
FHK2019 <48551681+FHK2019@users.noreply.github.com>
2020-04-25 23:52:48 +0800

    Call back example

---
+++ callback_example.js
@@ -0,0 +1,13 @@
+//This is how we used callbacks earlier before promises were 
+//introduced. This is how we do it in old school way. 
+function firstMethod(callBack) {
+    setTimeout(function () {
+        console.log("1");
+        callBack();
+    }, 1500);
+}
+function secondMethod() {
+    console.log("2");
+}
+firstMethod(secondMethod);
+//# sourceMappingURL=callback_example.js.map
---
+++ callback_example.ts
@@ -0,0 +1,15 @@
+//This is how we used callbacks earlier before promises were 
+//introduced. This is how we do it in old school way. 
+
+function firstMethod(callBack) {
+  setTimeout(() => {
+     console.log("1");
+     callBack();
+  }, 1500);
+}
+
+function secondMethod(){
+  console.log("2");
+}
+
+firstMethod(secondMethod);
